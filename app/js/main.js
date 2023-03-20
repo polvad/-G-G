da1f271ffcc157e081e457313d6caa645e077bda
Vadym <polvad2308@gmail.com>
2023-03-20 21:57:53 +0200

    advice

--- app/js/main.js
+++ app/js/main.js
@@ -15,3 +15,27 @@ const swiper = new Swiper('.card-product__slider', {
 
 });
 
+const caseBtn = document.querySelector('.showcase__btn-all');
+
+caseBtn.addEventListener('click', function () {
+    const showList = document.querySelector('.showcase__list-product')
+    const itemCard = document.querySelectorAll('.showcase__item-product');
+    itemCard.forEach(item => item.classList.toggle('show'));
+
+    this.remove()
+})
+
+const fieldAdvice = document.querySelectorAll('.form-advice__field');
+
+fieldAdvice.forEach(item => item.addEventListener('input', hint))
+
+function hint() {
+    const textHint = this.parentElement.querySelector('.form-advice__hint');
+    if (this.value.length > 0) {
+        textHint.classList.add('active');
+    } else {
+        textHint.classList.remove('active');
+
+    }
+
+}
